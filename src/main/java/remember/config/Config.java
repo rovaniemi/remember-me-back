1182c9550d15ea1db2c3e23eb8fe63f1b17126e7
Mauri Karlin <karlin@cs.helsinki.fi>
2017-11-27 19:52:08 +0200

    cors mapping get,post,put,delete

--- src/main/java/remember/config/Config.java
+++ src/main/java/remember/config/Config.java
@@ -14,7 +14,8 @@ public class Config {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                                    .allowedMethods("GET","PUT","POST","DELETE");
             }
         };
     }
